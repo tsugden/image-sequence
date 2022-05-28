af9ddb5d6b7e2f75b35c2f3b15a29840b23dddb4
Tom Sugden <tom@tomsugden.uk>
2022-05-28 20:19:11 +0100

    Tests updated.

---
+++ image_sequence/image_sequence.py
@@ -0,0 +1,9 @@
+"""Image Sequence"""
+
+
+from typing import List
+
+
+def sequence() -> List[int]:
+    """Return 1,2,3"""
+    return [1, 2, 3]
--- tests/test_image_sequence.py
+++ tests/test_image_sequence.py
@@ -1,5 +1,14 @@
+"""Tests for Image Sequence object."""
+
 from image_sequence import __version__
+from image_sequence.image_sequence import sequence
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """Test module version."""
+    assert __version__ == "0.1.0"
+
+
+def test_sequence():
+    """Test pytest"""
+    assert sequence() == [1, 2, 3]
